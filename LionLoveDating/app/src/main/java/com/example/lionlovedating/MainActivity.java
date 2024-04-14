d188270427aa61e88710d841ade02cb804dde899
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2024-04-14 15:48:36 -0400

    register matches to database

--- LionLoveDating/app/src/main/java/com/example/lionlovedating/MainActivity.java
+++ LionLoveDating/app/src/main/java/com/example/lionlovedating/MainActivity.java
@@ -76,14 +76,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
+               cards obj = (cards) dataObject;
+               String userId = obj.getUserId();
+               usersDb.child(oppositeUserSex).child(userId).child("connections").child("ewwno").child(currentUId).setValue(true);
                 Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                usersDb.child(oppositeUserSex).child(userId).child("connections").child("yusss").child(currentUId).setValue(true);
                 Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
             }
 
@@ -177,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         oppositeSexDb.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String a) {
-                if(dataSnapshot.exists()){
+                if(dataSnapshot.exists() && !dataSnapshot.child("connections").child("ewwno").hasChild(currentUId) && !dataSnapshot.child("connections").child("yusss").hasChild(currentUId) ){
 
                     cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString());
                     rowItems.add(item);
