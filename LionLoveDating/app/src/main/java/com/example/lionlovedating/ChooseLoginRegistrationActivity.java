435bb0574ffdd59802a0aeed0f98e563972b05d4
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2024-04-14 00:58:03 -0400

    ChooseLoginRegistrationActivity page update

---
+++ LionLoveDating/app/src/main/java/com/example/lionlovedating/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,51 @@
+package com.example.lionlovedating;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_choose_login_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
